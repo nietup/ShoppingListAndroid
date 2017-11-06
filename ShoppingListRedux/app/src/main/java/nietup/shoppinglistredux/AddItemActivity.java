69acdb67260419f42c0249d33811e3e7ee42954d
nietup <22.jakub@gmail.com>
2017-11-06 13:41:53 +0100

    basic add item activity design

--- ShoppingListRedux/app/src/main/java/nietup/shoppinglistredux/AddItemActivity.java
+++ ShoppingListRedux/app/src/main/java/nietup/shoppinglistredux/AddItemActivity.java
@@ -8,6 +8,6 @@ public class AddItemActivity extends SLRActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_add_item);
+        setContentView(R.layout.activity_add_item);
     }
 }
